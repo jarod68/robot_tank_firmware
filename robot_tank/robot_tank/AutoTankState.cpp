169829196cf6e22e626ea73709113ac13090bfd4
jarod68 <milou68@gmail.com>
2014-09-22 15:23:48 +0200

    Implement auto strategy algorithm base

--- robot_tank/robot_tank/AutoTankState.cpp
+++ robot_tank/robot_tank/AutoTankState.cpp
@@ -1,7 +1,7 @@
 #include "AutoTankState.h"
 
 
-AutoTankState::AutoTankState(Tank * tank) : TankState(tank)
+AutoTankState::AutoTankState(Tank * tank) : TankState(tank), _maxSpeed(DEFAULT_MAX_SPEED), _minSpeed(DEFAULT_MIN_SPEED), _securityDistance(DEFAULT_SECURITY_DISTANCE), _isTurning(false)
 {
     
 }
@@ -19,11 +19,24 @@ void AutoTankState::onLoop()
     
     long distance = tank->distanceCm();
     
-    if(distance <= 20){
-        tank->right();
+    if(distance <= _securityDistance){
+        
+        if(!_isTurning){
+            
+            if(random(2) == 0)
+                tank->right();
+            else
+                tank->left();
+            
+            _isTurning = true;
+        }
+        
     }
     else{
+        
         tank->forward();
+         _isTurning = false;
+        
     }
     
     
@@ -31,4 +44,28 @@ void AutoTankState::onLoop()
 void AutoTankState::onKeyPress()
 {
     
+}
+
+void AutoTankState::setMaxSpeed(uint16_t max)
+{
+    _maxSpeed = max;
+}
+
+ void AutoTankState::setMinSpeed(uint16_t min)
+{
+    _minSpeed = min;
+}
+
+ uint16_t AutoTankState::getMaxSpeed() const
+{
+    return _maxSpeed;
+}
+ uint16_t AutoTankState::getMinSpeed() const
+{
+    return _minSpeed;
+}
+
+uint16_t AutoTankState::getSecurityDistance() const
+{
+    return _securityDistance;
 }
--- robot_tank/robot_tank/AutoTankState.h
+++ robot_tank/robot_tank/AutoTankState.h
@@ -7,6 +7,11 @@
 #include "TankState.h"
 #include "Tank.h"
 
+#define DEFAULT_MAX_SPEED 100
+#define DEFAULT_MIN_SPEED 200
+#define DEFAULT_SECURITY_DISTANCE 37
+
+
 class AutoTankState : public TankState
 {
 public:
@@ -17,7 +22,19 @@ public:
     virtual void onLoop();
     virtual void onKeyPress();
     
+    virtual void setMaxSpeed(uint16_t max);
+    virtual void setMinSpeed(uint16_t min);
     
+    virtual uint16_t getMaxSpeed() const;
+    virtual uint16_t getMinSpeed() const;
+    virtual uint16_t getSecurityDistance() const;
+
+    
+private:
+    uint16_t _maxSpeed;
+    uint16_t _minSpeed;
+    uint16_t _securityDistance;
+    bool _isTurning;
 };
 
 #endif
