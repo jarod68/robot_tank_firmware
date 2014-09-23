8cd974050cb9ddb559292d14f26ee7de5ecb59dd
jarod68 <milou68@gmail.com>
2014-09-23 11:55:06 +0200

    Clean unused code

--- robot_tank/robot_tank/robot_tank.ino
+++ robot_tank/robot_tank/robot_tank.ino
@@ -24,71 +24,6 @@
 #include "Tank.h"
 #include "AutoTankState.h";
 
-/*
-
-MeDCMotor motor1(M1);
-MeDCMotor motor2(M2);
-MeUltrasonicSensor ultraSonic(PORT_3);
-
-
-uint8_t lineSpeed = 100;
-uint8_t turnSpeed = 200;
-long currentDistance;
-long previousDistance;
-long blockCalls = 0;
-bool isTurning = false;
-const uint16_t minDistance = 37;
-const uint16_t highSpeedDistance = 80;
-
-
-void forward(){
-    motor1.run(lineSpeed);
-	motor2.run(lineSpeed);
-
-}
-
-void backward(){
-    motor1.run(-lineSpeed);
-	motor2.run(-lineSpeed);
-
-}
-
-void left(){
-    motor1.run(-turnSpeed);
-	motor2.run(turnSpeed);
-
-}
-
-void right(){
-    motor1.run(turnSpeed);
-	motor2.run(-turnSpeed);
-
-}
-
-void stop(){
-    motor1.stop();
-	motor2.stop();
-    
-}
-
-void handleBlockDetection(){
-    
-    if(blockCalls >= 30){
-        previousDistance = currentDistance;
-    }
-    
-    else
-        blockCalls++;
-}
-
-bool isBlocked(){
-    
-    if (blockCalls >= 30 && sqrt(pow(currentDistance,2) + pow(previousDistance, 2)) <=minDistance)
-        return true;
-    
-    return false;
-}
-*/
 
 Tank tank (M1, M2, PORT_3);
 AutoTankState autoTankState(&tank);
@@ -98,36 +33,11 @@ void setup() {
     randomSeed(analogRead(PORT0));
     Serial.begin(9600);
 
-    
     tank.setCurrentState(&autoTankState);
 }
 
 void loop() {
     tank.onLoop();
     
-   // tank.onLoop();
-    /*
-    currentDistance =ultraSonic.distanceCm();
-    handleBlockDetection();
-    
-    if(isBlocked()){
-        
-        lineSpeed = 100;
-        backward();
-        delay(500);
-        
-    }else if(currentDistance<=minDistance ){
-        
-        left();
-        delay(500);
-    }else{
-        
-        if(currentDistance >=highSpeedDistance)
-            lineSpeed = 200;
-        else
-            lineSpeed = 100;
-        
-        forward();
-    }
-    */
+
 }
