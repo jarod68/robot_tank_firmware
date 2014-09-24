bd1034fb8940034a56172dbc317aab32441c79ff
jarod68 <milou68@gmail.com>
2014-09-24 21:13:55 +0200

    Re-indent full code in Tank

--- robot_tank/robot_tank/Tank.cpp
+++ robot_tank/robot_tank/Tank.cpp
@@ -3,20 +3,34 @@
 #include "ManualTankState.h"
 #include "AutoTankState.h"
 
-Tank::Tank (uint8_t rightMotorPort, uint8_t leftMotorPort, uint8_t ultraSonicSensorPort, uint8_t irReceiverPOrt) :
-_rightMotor(new MeDCMotor (rightMotorPort)), _leftMotor(new MeDCMotor (leftMotorPort)), _ultraSonicSensor(new MeUltrasonicSensor(ultraSonicSensorPort)), _currentState(NULL), _parkingState(NULL), _autoState(NULL), _manualState(NULL), _turnSpeed(DEFAULT_TURN_SPEED), _lineSpeed(DEFAULT_LINE_SPEED), _irReceiver(new MeInfraredReceiver(irReceiverPOrt)), _currentMove(Stop), _buzzerMode(None)
+    Tank    ::  Tank (uint8_t rightMotorPort,
+                      uint8_t leftMotorPort,
+                      uint8_t ultraSonicSensorPort,
+                      uint8_t irReceiverPOrt
+                      ) :
+                        _rightMotor(new MeDCMotor (rightMotorPort)),
+                        _leftMotor(new MeDCMotor (leftMotorPort)),
+                        _ultraSonicSensor(new MeUltrasonicSensor(ultraSonicSensorPort)),
+                        _currentState(NULL),
+                        _parkingState(NULL),
+                        _autoState(NULL),
+                        _manualState(NULL),
+                        _turnSpeed(DEFAULT_TURN_SPEED),
+                        _lineSpeed(DEFAULT_LINE_SPEED),
+                        _irReceiver(new MeInfraredReceiver(irReceiverPOrt)),
+                        _currentMove(Stop),
+                        _buzzerMode(None)
 {
-    //setCurrentState(new ParkingTankState(this));// we do that here because this is not initialized in the initilization part
-    
+    // Unsafe to use this in initializer list
     _parkingState   = new ParkingTankState  (this);
     _manualState    = new ManualTankState   (this);
     _autoState      = new AutoTankState     (this);
     
     setMode(Parking);
-    _irReceiver->begin();
+    _irReceiver->begin(); //start infrared receiver
 }
 
- Tank::~Tank ()
+    Tank    ::  ~Tank ()
 {
     delete _rightMotor;
     delete _leftMotor;
@@ -24,12 +38,12 @@ _rightMotor(new MeDCMotor (rightMotorPort)), _leftMotor(new MeDCMotor (leftMotor
     delete _irReceiver;
 }
 
-TankState * Tank::getCurrentState() const
+TankState *     Tank    ::  getCurrentState () const
 {
     return _currentState;
 }
 
-void Tank::setCurrentState(TankState * state)
+void            Tank    ::  setCurrentState (TankState * state)
 {
     if(state == NULL)
         return;
@@ -37,7 +51,7 @@ void Tank::setCurrentState(TankState * state)
     _currentState = state;
 }
 
-void Tank::onLoop()
+void            Tank    ::  onLoop          ()
 {
     if (_currentState == NULL)
         return;
@@ -50,109 +64,114 @@ void Tank::onLoop()
     delayMicroseconds(1);
 }
 
- void Tank::forward(uint8_t speed)
+void           Tank    ::  forward         (uint8_t speed)
 {
     this->setLineSpeed(speed);
     this->forward();
 }
- void Tank::forward()
+
+void           Tank    ::  forward         ()
 {
     _leftMotor->run(+_lineSpeed);
     _rightMotor->run(+_lineSpeed);
     _currentMove = Forward;
 }
 
- void Tank::backward(uint8_t speed)
+void           Tank     ::  backward           (uint8_t speed)
 {
     this->setLineSpeed(speed);
     this->backward();
 }
- void Tank::backward()
+
+ void           Tank    ::  backward    ()
 {
     _leftMotor->run(- _lineSpeed);
     _rightMotor->run(- _lineSpeed);
     _currentMove = Backward;
 }
 
- void Tank::left()
+void            Tank    ::  left        ()
 {
     _leftMotor->run(- _turnSpeed);
     _rightMotor->run(+_turnSpeed);
     _currentMove = Left;
 }
- void Tank::right()
+void            Tank    ::  right       ()
 {
     _leftMotor->run(+_turnSpeed);
     _rightMotor->run(-_turnSpeed);
     _currentMove = Right;
 }
 
-void Tank::stop()
+void            Tank    ::  stop        ()
 {
     _leftMotor->stop();
     _rightMotor->stop();
     _currentMove = Stop;
 }
 
- void Tank::setTurnSpeed(uint8_t turnSpeed)
+ void           Tank    ::  setTurnSpeed    (uint8_t turnSpeed)
 {
     _turnSpeed = turnSpeed;
 }
- void Tank::setLineSpeed(uint8_t lineSpeed)
+
+ void           Tank    ::  setLineSpeed    (uint8_t lineSpeed)
 {
     _lineSpeed = lineSpeed;
 }
 
- uint8_t Tank::getTurnSpeed() const
+ uint8_t        Tank    ::  getTurnSpeed    () const
 {
     return _turnSpeed;
 }
- uint8_t Tank::getLineSpeed() const
+
+ uint8_t        Tank    ::  getLineSpeed    () const
 {
     return _lineSpeed;
 }
 
-long Tank::distanceCm()const
+long            Tank    ::  distanceCm      () const
 {
     return _ultraSonicSensor->distanceCm();
 }
 
-TankMove Tank::getCurrentMove() const{
+TankMove        Tank    ::  getCurrentMove  () const
+{
     return _currentMove;
 }
 
- TankMode Tank::getCurrentMode() const
+ TankMode       Tank    ::  getCurrentMode  () const
 {
     return _currentMode;
 }
 
- void Tank::setMode(TankMode mode)
+void           Tank     ::  setMode         (TankMode mode)
 {
     _currentMode = mode;
     stop();
     setBuzzerMode(None);
     switch (mode) {
-        case Auto: setCurrentState(_autoState); break;
-        case Manual:setCurrentState(_manualState);break;
-        case Parking:setCurrentState(_parkingState);break;
-            
-        default:break;
+        case Auto   : setCurrentState(_autoState);      break;
+        case Manual : setCurrentState(_manualState);    break;
+        case Parking: setCurrentState(_parkingState);   break;
+        default     : break;
     }
     
 }
 
- BuzzerMode Tank::getBuzzerMode()const
+BuzzerMode      Tank    ::  getBuzzerMode   () const
 {
     return _buzzerMode;
 }
- void Tank::setBuzzerMode(BuzzerMode mode)
+
+void            Tank    ::  setBuzzerMode   (BuzzerMode mode)
 {
     _buzzerMode = mode;
     
     switch (mode) {
-        case None: buzzerOff(); break;
-        case Continue: buzzerOff(); break;
-        default:break;
+        case None       : buzzerOff(); break;
+        case Continue   : buzzerOff(); break;
+        default         : break;
     }
     
 }
--- robot_tank/robot_tank/Tank.h
+++ robot_tank/robot_tank/Tank.h
@@ -15,24 +15,23 @@
 class TankState;
 
 enum TankMove {
-    Forward,
-    Backward,
-    Left,
-    Right,
-    Stop
+        Forward,
+        Backward,
+        Left,
+        Right,
+        Stop
 };
 
 enum TankMode {
-    Auto,
-    Parking,
-    Manual
+        Auto,
+        Parking,
+        Manual
 };
 
 enum BuzzerMode {
-    None,
-    Alarm,
-    Continue
-    
+        None,
+        Alarm,
+        Continue
 };
 
 class Tank
@@ -40,58 +39,63 @@ class Tank
     
     
 public:
-    Tank (uint8_t rightMotorPort, uint8_t leftMotorPort, uint8_t ultraSonicSensorPort, uint8_t irReceiverPOrt);
-    virtual ~Tank ();
+            Tank    (uint8_t rightMotorPort,
+                     uint8_t leftMotorPort,
+                     uint8_t ultraSonicSensorPort,
+                     uint8_t irReceiverPOrt
+                     );
+    
+    virtual ~Tank   ();
     
    
     
-    virtual void onLoop();
+    virtual void onLoop     ();
     
-    virtual void forward(uint8_t speed);
-    virtual void forward() ;
+    virtual void forward    (uint8_t speed);
+    virtual void forward    ();
 
-    virtual void backward(uint8_t speed);
-    virtual void backward() ;
+    virtual void backward   (uint8_t speed);
+    virtual void backward   ();
 
-    virtual void left() ;
-    virtual void right() ;
+    virtual void left       ();
+    virtual void right      ();
     
-    virtual void stop() ;
+    virtual void stop       ();
 
-    virtual void setTurnSpeed(uint8_t turnSpeed);
-    virtual void setLineSpeed(uint8_t lineSpeed);
+    virtual void setTurnSpeed       (uint8_t turnSpeed);
+    virtual void setLineSpeed       (uint8_t lineSpeed);
     
-    virtual uint8_t getTurnSpeed() const;
-    virtual uint8_t getLineSpeed() const;
+    virtual uint8_t getTurnSpeed    () const;
+    virtual uint8_t getLineSpeed    () const;
     
-    virtual TankMove getCurrentMove() const;
-    virtual TankMode getCurrentMode() const;
+    virtual TankMove getCurrentMove () const;
+    virtual TankMode getCurrentMode () const;
     
-    virtual void setMode(TankMode mode);
+    virtual void setMode            (TankMode mode);
     
-    virtual long distanceCm()const;
+    virtual long distanceCm         () const;
     
-    virtual BuzzerMode getBuzzerMode()const;
-    virtual void setBuzzerMode(BuzzerMode mode);
+    virtual BuzzerMode getBuzzerMode() const;
+    virtual void setBuzzerMode      (BuzzerMode mode);
     
 protected:
-    virtual TankState * getCurrentState() const;
-    virtual void setCurrentState(TankState * state);
+    virtual TankState * getCurrentState () const;
+    virtual void setCurrentState        (TankState * state);
     
 private:
-    MeDCMotor * _rightMotor;
-    MeDCMotor * _leftMotor;
-    MeUltrasonicSensor * _ultraSonicSensor;
-    MeInfraredReceiver * _irReceiver;
-    TankState * _currentState;
-    TankState * _parkingState;
-    TankState * _autoState;
-    TankState * _manualState;
-    uint8_t _turnSpeed;
-    uint8_t _lineSpeed;
-    TankMove _currentMove;
-    TankMode _currentMode;
-    BuzzerMode _buzzerMode;
+    MeDCMotor           * _rightMotor;
+    MeDCMotor           * _leftMotor;
+    MeUltrasonicSensor  * _ultraSonicSensor;
+    MeInfraredReceiver  * _irReceiver;
+    TankState           * _currentState;
+    TankState           * _parkingState;
+    TankState           * _autoState;
+    TankState           * _manualState;
+    uint8_t               _turnSpeed;
+    uint8_t               _lineSpeed;
+    TankMove              _currentMove;
+    TankMode              _currentMode;
+    BuzzerMode            _buzzerMode;
 };
 
 
